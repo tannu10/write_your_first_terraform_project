09080f94e92954760c801e71dc84c594ff18323c
tannu10 <168409891+tannu10@users.noreply.github.com>
2024-12-19 23:47:11 +0530

    Update main.tf

--- aws/remote_state/main.tf
+++ aws/remote_state/main.tf
@@ -9,11 +9,11 @@
 # ----------------------------------------------------------------------------------------------------------------------
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
+    bucket         = "terraform-state10"
     key            = "some_environment/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "Terraform_db"
+    dynamodb_table = "terraform_db"
   }
 }
 
